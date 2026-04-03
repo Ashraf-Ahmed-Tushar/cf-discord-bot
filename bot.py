e399a98f2b6b5ad8dff7eefa60f3fb37be018220
Ashraf-Ahmed-Tushar <ashrafeusc100@gmail.com>
2026-04-04 00:04:25 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,21 @@ import asyncio
 import os
 from datetime import datetime, timezone
 import pytz
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=10000)
+
+def keep_alive():
+    t = threading.Thread(target=run)
+    t.start()
 
 TOKEN               = os.getenv("TOKEN")
 CHANNEL_ID          = int(os.getenv("CHANNEL_ID", "0"))
@@ -556,4 +571,5 @@ async def on_command_error(ctx, error):
 if not TOKEN:
     raise RuntimeError("TOKEN env variable is not set!")
 
+keep_alive()
 bot.run(TOKEN)
